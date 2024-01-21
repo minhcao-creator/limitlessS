2bc1a01ccea074b4bd47f77824a40fb96dc860c6
MinhCao <minhcao2612@gmail.com>
2024-01-21 12:53:20 +0700

    route

--- src/App.jsx
+++ src/App.jsx
@@ -4,44 +4,46 @@ import IdeaFulfill from './pages/IdeaFulfill'
 import TheNewMoney from './pages/theNewMoney'
 import EnhancePro from './pages/enhancepro'
 import * as ReactDOM from "react-dom/client";
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import {Routes, Route, Navigate} from "react-router-dom"
 
-const router = createBrowserRouter([
-  // {
-  //   path:"/aboutus",
-  //   element: <AboutUs />
-  // },
-  // {
-  //   path:"/contact",
-  //   element: <Contact />
-  // },
-  {
-    path:"/ideafulfill",
-    element: <IdeaFulfill />
-  },
-  {
-    path:"/thenewmoney",
-    element: <TheNewMoney />
-  },
-  {
-    path:"/enhancepro",
-    element: <EnhancePro />
-  },
-  // {
-  //   path:"/enhancepro",
-  //   element: <EnhancePro />
-  // },
-  {
-    path: "/",
-    element: <Home/>,
-  },
-]);
+// const router = createBrowserRouter([
+//   // {
+//   //   path:"/aboutus",
+//   //   element: <AboutUs />
+//   // },
+//   // {
+//   //   path:"/contact",
+//   //   element: <Contact />
+//   // },
+//   {
+//     path:"/ideafulfill",
+//     element: <IdeaFulfill />
+//   },
+//   {
+//     path:"/thenewmoney",
+//     element: <TheNewMoney />
+//   },
+//   {
+//     path:"/enhancepro",
+//     element: <EnhancePro />
+//   },
+//   // {
+//   //   path:"/enhancepro",
+//   //   element: <EnhancePro />
+//   // },
+//   {
+//     path: "/",
+//     element: <Home/>,
+//   },
+// ]);
 
 const App = () => (
-  <RouterProvider router={router} />
+  <Routes>
+    <Route path="/enhancepro" element={ <EnhancePro/>}/>
+    <Route path="/thenewmoney" element={ <TheNewMoney/>}/>
+    <Route path="/ideafulfill" element={<IdeaFulfill/>}/>
+    <Route path="/" element={<Home/>}/>
+  </Routes>
   )
 
 export default App
